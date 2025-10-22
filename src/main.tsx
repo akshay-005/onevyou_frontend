2930d749906606ddbf21c2f632495c68e7532e06
Akshay Dev <akshaydevktk@gmail.com>
2025-10-22 23:35:27 +0530

    socket removed from main.tsx; only using App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { SocketProvider } from "@/utils/socket";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SocketProvider>
-      <App />
-    </SocketProvider>
+    <App />
   </React.StrictMode>
 );
