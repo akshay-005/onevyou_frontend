97e7080ddabb3d9aa0a41b2b311da099fda5b3f0
Akshay Dev <akshaydevktk@gmail.com>
2025-10-28 15:26:14 +0530

    Notification badge restoration: debugging 02

--- src/pages/UnifiedDashboard.tsx
+++ src/pages/UnifiedDashboard.tsx
@@ -54,7 +54,7 @@ import { useToast } from "@/hooks/use-toast";
 import { useSocket } from "@/utils/socket";
 import api from "@/utils/api";
 
-import IncomingCallModal from "@/components/IncomingCallModal";
+//import IncomingCallModal from "@/components/IncomingCallModal";
 import PricingModal from "@/components/PricingModal";
 import NotificationPanel from "@/components/NotificationPanel";
 import EarningsCard from "@/components/EarningsCard";
